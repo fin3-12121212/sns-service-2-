6bfd8971b68aa151da3f1c623b76a996696e5513
fin3-12121212 <115126130+fin3-12121212@users.noreply.github.com>
2024-06-17 16:12:25 +0900

    Update board.js

---
+++ models/board.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const boardSchema = new mongoose.Schema({
+    name: { type: String, required: true, unique: true },
+    description: { type: String },
+    created_at: { type: Date, default: Date.now },
+    updated_at: { type: Date, default: Date.now }
+});
+
+const Board = mongoose.model('Board', boardSchema);
+
+module.exports = Board;
