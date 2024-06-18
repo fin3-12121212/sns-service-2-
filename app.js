93394308c37687bc834d48eb49daba132bc1c8e6
fine <otxw0125@gmail.com>
2024-06-19 03:49:03 +0900

    일단락

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mongoose = require('mongoose');
-var jwt = require('jsonwebtoken');
-require('dotenv').config(); // dotenv 로드
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const postsRouter = require('./routes/posts'); // posts 라우트 추가
-const boardsRouter = require('./routes/boards'); // boards 라우트 추가
-const youtubeRouter = require('./routes/youtube'); // youtube 라우트 추가
-const mainRouter = require('./routes/main'); // main 라우트 추가
+const postsRouter = require('./routes/posts');
+const boardsRouter = require('./routes/boards');
 
-var app = express();
+const app = express();
 
 // MongoDB 연결 설정
 mongoose.connect('mongodb+srv://bob:makeit123@fine.mngnqv4.mongodb.net/post?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
 })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
@@ -47,11 +42,10 @@ app.use(async (req, res, next) => {
   next();
 });
 
-app.use('/', mainRouter); // main 라우트 사용
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/posts', postsRouter); // posts 라우트 사용
-app.use('/boards', boardsRouter); // boards 라우트 사용
-app.use('/youtube', youtubeRouter); // youtube 라우트 사용
+app.use('/posts', postsRouter);
+app.use('/boards', boardsRouter);
 
 // 루트 경로로 요청이 들어오면 /main.html로 리다이렉트
 app.get('/', (req, res) => {
@@ -65,11 +59,9 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
