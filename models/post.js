ccd996b03671f36809e74760c481c820ff6c7c4c
fine <otxw0125@gmail.com>
2024-06-18 11:27:52 +0900

    resolve thing

--- models/post.js
+++ models/post.js
@@ -2,13 +2,14 @@ const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
     user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    board: { type: String, required: true }, // 게시판 정보 추가
     title: { type: String, required: true },
     content: { type: String, required: true },
     created_at: { type: Date, default: Date.now },
     updated_at: { type: Date, default: Date.now }
 });
-// 텍스트 인덱스 추가
-postSchema.index({ title: 'text', content: 'text' });
+
+postSchema.index({ title: 'text', content: 'text' }); // 텍스트 인덱스 추가
 
 const Post = mongoose.model('Post', postSchema);
 
