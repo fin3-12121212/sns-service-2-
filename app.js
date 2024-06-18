aca01ccdfa0df18f65c6de7c54684cb7de279b7f
fine <otxw0125@gmail.com>
2024-06-19 03:41:04 +0900

    메인 1차 수정

--- app.js
+++ app.js
@@ -20,8 +20,6 @@ var app = express();
 mongoose.connect('mongodb+srv://bob:makeit123@fine.mngnqv4.mongodb.net/post?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  connectTimeoutMS: 30000, // 30초로 타임아웃 설정
-  socketTimeoutMS: 45000, // 45초로 소켓 타임아웃 설정
 })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
@@ -54,6 +52,11 @@ app.use('/users', usersRouter);
 app.use('/posts', postsRouter); // posts 라우트 사용
 app.use('/boards', boardsRouter); // boards 라우트 사용
 app.use('/youtube', youtubeRouter); // youtube 라우트 사용
+
+// 루트 경로로 요청이 들어오면 /main.html로 리다이렉트
+app.get('/', (req, res) => {
+  res.redirect('/main.html');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
